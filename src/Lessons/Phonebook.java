15209254bde84b857f6ddffc86f2353523a6f99f
crangoren <crangoren@gmail.com>
2021-05-04 20:03:25 +0300

    initial commit

---
+++ src/Lessons/Phonebook.java
@@ -0,0 +1,33 @@
+package Lessons;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Phonebook {
+    private Map<Integer, String> phonebook;
+
+    Phonebook() {
+        phonebook = new HashMap<>();
+    }
+
+    public void add(int number, String name) {
+        phonebook.put(number, name);
+    }
+
+    public void get(String name) {
+
+
+            if (phonebook.containsValue(name)) {
+                Set<Map.Entry<Integer, String>> set = phonebook.entrySet();
+                for (Map.Entry<Integer, String> temp : set) {
+                    if (temp.getValue().equals(name)) {
+                        System.out.println(temp.getValue() + " : " + temp.getKey());
+                    }
+                }
+
+            }
+
+
+    }
+}
